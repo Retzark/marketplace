9b5c364a6e3e8859c75a65ea89ea966eacf335c7
Gerard Baluyot <gacbaluyot@gmail.com>
2024-04-29 16:37:13 +0800

    added buy packs

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
+import userStore from "@/store/userStore";
+import useAppStore from "@/store/useAppStore";
+import useStore from "@/store";
 
-// Create an Axios instance specifically for API requests
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL, // Ensure the baseURL is correctly set from environment variables
+  withCredentials: true
 });
 
 const apiService = {
@@ -18,16 +21,52 @@ const apiService = {
     }
   },
   async startPurchase() {
-    const payload = {
-      username: "gacbaluyot",
-      items: [{ symbol: "DATA", quantity: 1 }],
-      payment_method: "crypto",
-      currency: "HIVE",
+    const username = userStore.getState().user.username;
+    const payload ={
+      "username":username,
+      "items":[{"symbol":"DATA","quantity":1}],
+      "payment_method":"crypto",
+      "currency":"HBD"
     };
 
+
+
     try {
-      const response = await api.post("/purchases/start", payload, {});
-      console.log(response.data);
+      // Create an Axios instance specifically for API requests
+      const response = await axios.post(`https://market.retzark.com/api/purchases/start`, payload , {});
+      let operations = []
+
+      const paymentInfo = response.data;
+      const { sidechain_id } = useAppStore.getState().settings;
+
+      // console.log(paymentInfo)
+      if (paymentInfo.payment_info.type === 'hive') {
+        operations = [['transfer', {
+          from: username,
+          to: paymentInfo.payment_info.address,
+          amount: `${paymentInfo.payment_info.amount} ${paymentInfo.payment_info.currency}`,
+          memo: paymentInfo.payment_info.memo
+        }]]
+      } else {
+        operations = [['custom_json', {
+          required_auths: [username],
+          required_posting_auths: [],
+          id: sidechain_id,
+          json: JSON.stringify({
+            contractName: 'tokens',
+            contractAction: 'transfer',
+            contractPayload: {
+              symbol: paymentInfo.currency,
+              quantity: paymentInfo.payment.amount.toString(),
+              to: paymentInfo.address,
+              memo: paymentInfo.uid
+            }
+          })
+        }]]
+      }
+      const broadcastOps = useStore.getState().requestBroadcastOps;
+      await broadcastOps(username, operations, "Active"); // console.log("Packs opened successfully:", result.data);
+
     } catch (error) {
       console.error("Error making the purchase:", error);
     }
